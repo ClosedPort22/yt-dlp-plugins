9f1d7ee675390a4e78cae05f63eddd3bb28ee208
ClosedPort22 <44864697+ClosedPort22@users.noreply.github.com>
2026-08-09 13:23:38 +0800

    [applemusic] fix anonymous token extraction

--- yt_dlp_plugins/extractor/applemusic.py
+++ yt_dlp_plugins/extractor/applemusic.py
@@ -98,7 +98,7 @@ class AppleMusicBaseIE(InfoExtractor):
         webpage = self._download_webpage('https://beta.music.apple.com/', video_id, 'Retrieving anonymous token')
         path = self._search_regex(r'/(assets/index~[^/]+\.js)', webpage, name='path to JavaScript file')
         js = self._download_webpage(f'https://beta.music.apple.com/{path}', video_id, 'Downloading JavaScript file')
-        return self._search_regex(r'"(eyJh[^"]+)', js, 'anonymous token')
+        return self._search_regex(r'"(eyJ0[^"]+)', js, 'anonymous token')
 
     def _download_api_json(self, *args, expected_status=None, headers={}, **kwargs):  # noqa: B006
         kwargs.setdefault('transform_source', lambda x: x or '{}')
